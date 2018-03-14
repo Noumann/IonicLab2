12222a2a759cf95124158c9186d3b7b8834bea30
Noumann <32297685+Noumann@users.noreply.github.com>
2018-03-14 15:47:52 +0000

    Ionic lab 2 done!

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MovieProvider } from '../../providers/movie/movie';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+movies:any[]=[];
+  constructor(public navCtrl: NavController,private mp:MovieProvider) {
 
-  constructor(public navCtrl: NavController) {
-
+  }
+  
+  ionViewDidLoad(){
+    this.mp.GetMovieData().subscribe(data=>{
+      this.movies=data.Search;
+    })
   }
 
 }
